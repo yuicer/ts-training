4ca91178e9450d301ecc20d9c22d066f16bbbb64
yuicer <yuicer1@gmail.com>
2022-09-21 12:02:36 +0800

    ~

--- others/big-number-sum/index.test.ts
+++ others/big-number-sum/index.test.ts
@@ -2,4 +2,6 @@ import { bigNumberAdd } from './index';
 
 it('big number add', () => {
   expect(bigNumberAdd('1234', '56789')).toEqual('58023');
+  expect(bigNumberAdd('0', '56789')).toEqual('56789');
+  expect(bigNumberAdd('99999', '1111')).toEqual('101110');
 });
--- others/big-number-sum/index.ts
+++ others/big-number-sum/index.ts
@@ -9,31 +9,20 @@ function bigNumberAdd(str1: string, str2: string): string {
   let cache = 0;
   let curIndex = 0;
 
-  while (curIndex < Math.min(length1, length2)) {
-    const a = +strArr1[curIndex];
-    const b = +strArr2[curIndex];
+  while (curIndex < Math.max(length1, length2)) {
+    const a = strArr1[curIndex] === undefined ? 0 : +strArr1[curIndex];
+    const b = strArr2[curIndex] === undefined ? 0 : +strArr2[curIndex];
+
     const num = a + b + cache;
-    cache = Math.floor(num / 10);
     const c = num % 10;
+    cache = num >= 10 ? 1 : 0;
     result = c + result;
 
     curIndex++;
   }
 
-  if (length1 === length2) {
-    if (cache) result = cache + result;
-  }
-
-  if (length1 > length2) {
-    if (cache)
-      result =
-        bigNumberAdd(`${cache}`, str1.slice(0, length1 - curIndex)) + result;
-    else result = str1.slice(0, length1 - curIndex) + result;
-  } else if (length1 < length2) {
-    if (cache)
-      result =
-        bigNumberAdd(`${cache}`, str2.slice(0, length2 - curIndex)) + result;
-    else result = str2.slice(0, length2 - curIndex) + result;
+  if (cache) {
+    result = cache + result;
   }
 
   return result;
